3c2088d10e131352f36816e7012b0fefa0ba1ced
Paul Devitt <pauldev@google.com>
2015-11-06 14:23:40 -0800

    Lint Auth code

--- authentication.py
+++ authentication.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import getpass
-import keyring
 import json
 import time
+
+import keyring
 import requests
 
 
@@ -22,19 +23,21 @@ class AuthToken:
     return {'apikey': apikey, 'username': username, 'userpass': str(password)}
 
   def getrefreshtoken(self):
-    """ Get a JWT token or refreshes if it exists and is less than an hour."""
+    """Get a JWT token or refreshes if it exists and is less than an hour."""
     tvdb_url = 'https://api-beta.thetvdb.com/'
     headers = {'content-type': 'application/json'}
     lastrefresh = None
-    if lastrefresh == None:
-      print "Obtaining NEW token"
+    if lastrefresh is None:
+      print 'Obtaining NEW token'
       token = requests.post(tvdb_url + 'login',
-                  data=json.dumps(self.login_schema()), headers=headers)
+                            data=json.dumps(self.login_schema()),
+                            headers=headers)
       lastrefresh = time.time()
     elif lastrefresh < lastrefresh + 3600:
-      print "Obtaining REFRESH Token"
+      print 'Obtaining REFRESH Token'
       token = requests.get(tvdb_url + 'login',
-                 data=json.dumps(self.login_schema()), headers=headers)
+                           data=json.dumps(self.login_schema()),
+                           headers=headers)
       lastrefresh = time.time()
     finaltoken = json.loads(token.text)
     return finaltoken['token']
