16e387b9d3ade8fb2b56eac184195a393ce2d892
Paul Devitt <pauldev@google.com>
2015-12-02 17:11:04 -0800

    Fixed and added tests

--- renamer_test.py
+++ renamer_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import mock
 import unittest
 
 import renamer
@@ -14,7 +15,8 @@ class KnownValues(unittest.TestCase):
 
   goodids = (('Pokemon - S01E01 -.mp4', ('Pokemon', '01', '01')),
              ('Red Dwarf - S08E03 -.mp4', ('Red Dwarf', '08', '03')),
-             ('Black Books - S03E02 -.mp4', ('Black Books', '03', '02')))
+             ('Black Books - S03E02 -.mp4', ('Black Books', '03', '02')),
+             ('Hawaii Five-0 - S02E10 -.mp4', ('Hawaii Five-0', '02', '10')))
 
   goodfilenames = ((('Pokemon', '01', '01'), u'Pok\xe9mon! I Choose You!'),
                    (('Red Dwarf', '08', '03'), 'Back in the Red (3)'),
@@ -38,23 +40,52 @@ class KnownValues(unittest.TestCase):
       result = tvshow.episodename(fileids[0], fileids[1], fileids[2])
       self.assertEqual(episodename, result)
 
-  def empty_episode(self):
-    """episodename should do nothing if empty."""
-    result = tvshow.episodename(fileids[0])
-    self.assertEqual(None, result)
-
 
 class BadInput(unittest.TestCase):
 
+  badids = (('Pokemon', '01', '99'),
+             ('Red Dwarf', '40', '01',),
+             ('Blackadder', '1', '12'))
+
+  badnames = (('Rad Dwarf'),
+              ('Pokereman'),
+              ('Blackudder'))
+
+  noepisodeinseason = (('The Simpsons', '15', '36'),
+                       ('Stargate SG-1', '20', '32'),
+                       ('Star Trek - Voyager', '01', '400'))
+
+  
   def test_no_ep_in_season(self):
     """episodename should return None when the episode doesn't exist."""
-    result = tvshow.episodename('Red Dwarf', '01', '10')
-    self.assertIsNone(result)
+    for show, season, episode in self.badids:
+      result = tvshow.episodename(show, season, episode)
+      self.assertIsNone(result)
     
   def test_no_series_found(self):
     """searchseries should return None when no series found."""
-    result = tvshow.searchseries('Rad Dwarf')
-    self.assertIsNone(result)
+    for filename in self.badnames:
+      result = tvshow.searchseries(filename)
+      self.assertIsNone(result)
+
+  def test_empty_episode(self):
+    """episodename should do nothing if empty."""
+    for show, season, episode in self.noepisodeinseason:
+      result = tvshow.episodename(show, season, episode)
+      self.assertEqual(None, result)
+
+class testauthtokens(unittest.TestCase):
+
+  @patch.object('renamer.requests.get')
+  def test_invalid_token(self, mock_get):
+    """Tests the return value of 401 for missing token."""
+    mock_response = mock.Mock()
+    mock_response.status_code.return_value = 401
+    
+      
+
+
+
 
 if __name__ == '__main__':
   unittest.main()
